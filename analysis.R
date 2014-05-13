7980a1ded03fca5913b280576a7dfa5c7cf46a34
Andrew Dick <akj.dick@gmail.com>
2014-05-13 19:41:01 +1000

    added download functionality

--- analysis.R
+++ analysis.R
@@ -1,1 +1,6 @@
 # download data
+
+download.file(url="https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip", 
+              destfile="../data/activitydata.zip", 
+              method="curl")
+unzip(zipfile="../data/activitydata.zip", exdir="../data/", overwrite=TRUE)
